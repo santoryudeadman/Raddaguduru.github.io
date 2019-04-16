4c13f49d0c537aec6793a4fd9e81f48576834189
Raddaguduru <raviteja0628@gmail.com>
2019-04-16 03:23:17 -0500

    new theme

--- phoenix-v1.5/template-page-boxed.php
+++
@@ -1,3 +0,0 @@
-<?php
-/* Template Name: Boxed*/
-get_template_part('page');
--- phoenix-v1.5/template-page-nobg.php
+++
@@ -1,6 +0,0 @@
-<?php
-/* Template Name: Transparent BG */
-global $fwp_pageClass;
-
-$fwp_pageClass = 'no-bg-color';
-get_template_part('template-page-full');
